28f4a35f654e7ef1712aaf0da6617c225a00d9c8
Vitaliy-Future-Developer <zarvoron@yandex.ru>
2022-09-05 02:02:50 +0300

    tests for Node class

--- test/node.spec.js
+++ test/node.spec.js
@@ -1,1 +1,27 @@
 // You're lucky, no tests for node, do whatever you want!
+
+const Node = require('../src/node');
+
+describe('#constructor node', () => {
+	const nodePrev = new Node(110, null, null);
+	const nodeNext = new Node(120, null, null);
+	const node = new Node(100, nodePrev, nodeNext);
+
+	it('PrevNode is exist', () => {
+		expect(node.prev).to.not.be.null;
+		expect(node.prev).to.not.be.undefind;
+	});
+	it('NextNode is exist', () => {
+		expect(node.next).to.not.be.null;
+		expect(node.next).to.not.be.undefind;
+	});
+	it('Node object data', () => {
+		expect(node.data).to.equal(100);
+	});
+	it('Node object prev data', () => {
+		expect(node.prev.data).to.equal(110);
+	});
+	it('Node object next data', () => {
+		expect(node.next.data).to.equal(120);
+	})
+});
